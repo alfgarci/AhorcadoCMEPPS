5620f6adb964acf2c9930f04bf6a07f37998d14b
Alfonso García <alfgarci@Alfonsos-MacBook-Pro.local>
2024-12-16 14:36:35 +0100

    Desing

---
+++ src/main/java/aplications/ahoracado/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aplications.ahoracado;
+
+/**
+ *
+ * @author alfgarci
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
